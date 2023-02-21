8959156de0f11c5b50e4d66fab7a728461d86640
aayushdhakal69 <therespawner69@gmail.com>
2023-02-21 09:06:11 +0545

    FINAL 1

--- projectm1/settings.py
+++ projectm1/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = 'django-insecure-$7ii$%6+ey(dka0pqme#ul-ozitwfk5pt5ka4$(-*8gd2=dcq(
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.254.183',
-    '127.0.0.1',
-    '192.168.56.1'
+    '*'
 ]
 
 
